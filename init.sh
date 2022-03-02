9a62272d61aa9d37f498a9702e882198a1cd5248
daemon <59494058+le3ch-tech@users.noreply.github.com>
2022-03-02 05:38:58 +0100

    Delete init.sh

--- init.sh
+++
@@ -1,7 +0,0 @@
-echo "# le3ch-tech.github.com" >> README.md
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/le3ch-tech/le3ch-tech.github.com.git
-git push -u origin main
